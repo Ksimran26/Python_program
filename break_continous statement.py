536ee12497718650c5d4fd2f9937edeec78339e2
Ksimran26 <102537455+Ksimran26@users.noreply.github.com>
2022-05-02 23:47:48 +0530

    Add files via upload

---
+++ break_continous statement.py
@@ -0,0 +1,19 @@
+for i in range(0,11):
+    print (i)
+
+
+for i in range(0,11):
+    if i>5:
+        break
+    print (i)
+##It will break at 5 and don not show any no.greater than 5.
+
+for i in range(0,11):
+    if i<4:
+        continue
+    print (i)
+    
+##It willnot show no.less than 4 and continue/display\
+##   no. greater than equal to 4
+
+
