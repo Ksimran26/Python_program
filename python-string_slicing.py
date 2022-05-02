ebcb312613c7eebb49bd65ac8a18cc9a873330a3
Ksimran26 <102537455+Ksimran26@users.noreply.github.com>
2022-05-02 23:56:07 +0530

    Add files via upload

---
+++ python-string_slicing.py
@@ -0,0 +1,28 @@
+var = "Simran"
+print(var[0])
+print(var[-6])
+
+##S - 0,-6
+##i - 1,-5
+##m - 2,-4
+##r - 3,-3
+##a - 4,-2
+##n - 5,-1
+
+var = "dhoni is csk captain"  #string slicing can't happen on reverse direction.
+print(var[0])
+print(var[0:1])
+print(var[0:5])
+print(var[1:-1])
+
+
+
+str = "This is a nice watch."
+
+print(str)
+print(str[0:15])
+print(str[10:19])
+print(str[:19])#from strting
+
+
+
