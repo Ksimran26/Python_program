58e67732faad24c7409335dbf9c82e1eeac607b0
Ksimran26 <102537455+Ksimran26@users.noreply.github.com>
2022-05-07 13:59:23 +0530

    Add files via upload

---
+++ python_list.py
@@ -0,0 +1,23 @@
+var = []
+print(type(var))
+
+var = ["a","b",2,4,5.6]
+print(type(var))
+
+var = ["a","b",2,4,5.6]
+print(var[4])
+
+var = ["a","b",2,4,5.6]
+var[0] = "Simran"
+print(var)
+
+var = ["a","b",2,4,5.6]
+var.append("dhoni")
+print(var)
+
+var = ["ant","india","bat","cricket","zoom"]
+output = sorted(var)
+print(output)
+
+
+
